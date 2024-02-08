a35e366c39323a1e173f6ddf70e6e4703c05a474
Prakhyat Shrestha <81764309+prakh-yat@users.noreply.github.com>
2024-02-08 12:55:46 +0545

    Added

---
+++ APIGATEWAY/src/test/java/com/micro/gateway/APIGATEWAY/ApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.gateway.APIGATEWAY;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
